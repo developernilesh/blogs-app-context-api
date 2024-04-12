e2aa981c58e5884e7f1feda73ef88dea5f9dc0c2
developernilesh <nileshmukherjee2017@gmail.com>
2024-04-13 00:02:10 +0530

    route pathe correction

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/blogs-app-context-api">
         <AppContextProvider>
             <App />
         </AppContextProvider>
